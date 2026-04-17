747470d1d81f9f3e08865bc93b5300adc86ed8a4
Gyeore Kim <h2weirdo@icloud.com>
2026-04-17 14:37:10 +0900

    chore: JWT SecurityScheme 및 글로벌 SecurityRequirement 설정

--- src/main/java/com/groute/groute_server/common/config/SwaggerConfig.java
+++ src/main/java/com/groute/groute_server/common/config/SwaggerConfig.java
@@ -1,12 +1,23 @@
 package com.groute.groute_server.common.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class SwaggerConfig {
 
     @Bean
@@ -15,7 +26,8 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("GLIT API")
                         .description("GLIT 백엔드 API 문서")
-                        .version("v1.0.0"));
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 
     @Bean
