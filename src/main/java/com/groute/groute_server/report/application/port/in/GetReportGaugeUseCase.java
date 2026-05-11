83fd221a9d6a6518485c4a69d0641c7c87730aba
박도현 <parkdo0918@gmail.com>
2026-05-11 21:31:38 +0900

    feat: report 조회 usecase 추가

---
+++ src/main/java/com/groute/groute_server/report/application/port/in/GetReportGaugeUseCase.java
@@ -0,0 +1,17 @@
+package com.groute.groute_server.report.application.port.in;
+
+import com.groute.groute_server.report.adapter.in.web.dto.ReportGaugeResponse;
+
+/**
+ * RPT-001: 리포트 게이지 조회 유스케이스.
+ *
+ * <p>마지막 리포트 생성 이후 완료된 심화기록 수와 다음 생성 임계치를 반환한다.
+ */
+public interface GetReportGaugeUseCase {
+
+    /**
+     * @param userId 현재 로그인한 유저 ID
+     * @return 게이지 정보 (currentCount, nextThreshold, progressRate, isGeneratable)
+     */
+    ReportGaugeResponse getGauge(Long userId);
+}
