6a51dfe54816ae5756cd056d5fd3f39975c9a4ff
hyhy-j <lhjlhj10088@gmail.com>
2026-05-15 12:17:23 +0900

    refactor: OnboardingStatusResponse 필드명을 isOnboardingCompleted로 변경

--- src/main/java/com/groute/groute_server/user/dto/OnboardingStatusResponse.java
+++ src/main/java/com/groute/groute_server/user/dto/OnboardingStatusResponse.java
@@ -4,4 +4,4 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 /** 온보딩 완료 여부 조회(ONB-STATUS) 응답 DTO. */
 public record OnboardingStatusResponse(
-        @Schema(description = "온보딩 완료 여부", example = "true") boolean isCompleted) {}
+        @Schema(description = "온보딩 완료 여부", example = "true") boolean isOnboardingCompleted) {}
