10ce4c7a8d769b5946140cc5859c93c8f2154e50
hyhy-j <lhjlhj10088@gmail.com>
2026-05-20 16:00:16 +0900

    refactor: 이미지 업로드·confirm 커맨드를 배열 단위로 변경

--- src/main/java/com/groute/groute_server/record/application/port/in/star/UploadStarImageUseCase.java
+++ src/main/java/com/groute/groute_server/record/application/port/in/star/UploadStarImageUseCase.java
@@ -1,6 +1,8 @@
 package com.groute.groute_server.record.application.port.in.star;
 
+import java.util.List;
+
 public interface UploadStarImageUseCase {
 
-    UploadStarImageResult upload(UploadStarImageCommand command);
+    List<UploadStarImageResult> upload(UploadStarImageCommand command);
 }
