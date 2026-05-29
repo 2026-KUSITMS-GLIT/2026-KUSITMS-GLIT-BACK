1151e6b26e467a87eca5a639086de7cd6e3f87af
박도현 <parkdo0918@gmail.com>
2026-05-30 00:12:38 +0900

    chore: CI 테스트 활성화 및 JaCoCo 커버리지 게이트 적용

--- src/test/java/com/groute/groute_server/GrouteServerApplicationTests.java
+++ src/test/java/com/groute/groute_server/GrouteServerApplicationTests.java
@@ -1,9 +1,7 @@
 package com.groute.groute_server;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class GrouteServerApplicationTests {
 
     @Test
