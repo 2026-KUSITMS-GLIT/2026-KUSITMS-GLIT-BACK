622414674e11e57cd8f3fef3adea7b9b73903fb6
hyhy-j <lhjlhj10088@gmail.com>
2026-06-04 23:29:15 +0900

    chore: 마이그레이션 롤백 전략 주석 추가

--- src/main/resources/db/migration/V20260604041057__fix_projects_unique_index_partial.sql
+++ src/main/resources/db/migration/V20260604041057__fix_projects_unique_index_partial.sql
@@ -1,5 +1,11 @@
 -- (user_id, name, is_deleted) 전체를 unique key로 잡던 기존 인덱스를
 -- 활성 row(is_deleted=false)만 unique를 보장하는 부분 인덱스로 전환한다.
+--
+-- 롤백 방법:
+--   DROP INDEX uq_projects_user_name_active;
+--   CREATE UNIQUE INDEX uq_projects_user_name_is_deleted ON projects (user_id, name, is_deleted);
+--
+-- 주의: projects 테이블이 크다면 트래픽이 적은 시간대에 배포 권장 (SHARE 락 획득).
 
 DROP INDEX uq_projects_user_name_is_deleted;
 
